e4236ecd4208839467de9825ce26c9fa5b55a393
wildnhl <minnesotanhlwild@gmail.com>
2024-03-16 19:25:12 +0300

    feat(kino): add link to home in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,14 @@
+import { Link } from 'react-router-dom';
 import { Search } from './search/Search';
 import cl from './header.module.scss';
 
 export function Header() {
   return (
-    <header className="d-flex align-items-center">
-      <div className={cl.logo}>
+    <header className="d-flex align-items-center mb-5">
+      <Link to="/" className={cl.logo}>
         <span className={cl.moviesStart}>Mov</span>
         <span className={cl.moviesEnd}>ies</span>
-      </div>
+      </Link>
       <Search />
     </header>
   );
