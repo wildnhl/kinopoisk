e444ab1e1c59826d77cf6c0a86842c8edeebb0ac
wildnhl <minnesotanhlwild@gmail.com>
2024-03-19 20:42:38 +0300

    refactor(kino): refactor componnent

--- src/components/SingleMovie.tsx
+++ src/components/SingleMovie.tsx
@@ -1,20 +1,24 @@
+import { useAppSelector } from '../redux/store';
 import cl from '../styles/singleMovie.module.scss';
-import type { ISingleMovie } from '../types/singleMovieTypes';
 
-export function SingleMovie(props: ISingleMovie) {
-  if (props.error) {
-    return <div>Error: {props.error}</div>;
+export function SingleMovie() {
+  const movie = useAppSelector((state) => state.singleMovie.movieData);
+  const movieError = useAppSelector((state) => state.singleMovie.error);
+  const movieIsLoading = useAppSelector((state) => state.singleMovie.isLoading);
+
+  if (movieError) {
+    return <div>Error: {movieError}</div>;
   }
-  if (props.isLoading) {
+  if (movieIsLoading || !movie) {
     return <div>Loading...</div>;
   }
   return (
     <div className={cl.outer}>
-      <img className={cl.image} src={props.Poster} alt={props.Title} />
+      <img className={cl.image} src={movie.Poster} alt={movie.Title} />
       <div>
-        <p>{props.Genre}</p>
-        <h2 className={cl.title}>{props.Title}</h2>
-        <p className={cl.plot}>{props.Plot}</p>
+        <p>{movie.Genre}</p>
+        <h2 className={cl.title}>{movie.Title}</h2>
+        <p className={cl.plot}>{movie.Plot}</p>
         <div className="d-flex gap-5 mt-5">
           <div className="d-flex flex-column gap-3">
             <p className={cl.infoTitle}>Year</p>
@@ -25,12 +29,12 @@ export function SingleMovie(props: ISingleMovie) {
             <p className={cl.infoTitle}>Writers</p>
           </div>
           <div className="d-flex flex-column gap-3">
-            <p>{props.Year}</p>
-            <p>{props.Released}</p>
-            <p>{props.Country}</p>
-            <p>{props.Actors}</p>
-            <p>{props.Director}</p>
-            <p>{props.Writer}</p>
+            <p>{movie.Year}</p>
+            <p>{movie.Released}</p>
+            <p>{movie.Country}</p>
+            <p>{movie.Actors}</p>
+            <p>{movie.Director}</p>
+            <p>{movie.Writer}</p>
           </div>
         </div>
       </div>
--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -1,20 +1,16 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../redux/store';
-import { fetchSingleMovieThunk } from '../redux/single-movie-slice';
 import { SingleMovie } from '../components/SingleMovie';
+import { useAppDispatch } from '../redux/store';
+import { fetchSingleMovieThunk } from '../redux/single-movie-slice';
 
 export function MoviePage() {
   const dispatch = useAppDispatch();
   const { id } = useParams();
 
-  const movie = useAppSelector((state) => state.singleMovie.movieData);
-  const movieError = useAppSelector((state) => state.singleMovie.error);
-  const movieIsLoading = useAppSelector((state) => state.singleMovie.isLoading);
   useEffect(() => {
     dispatch(fetchSingleMovieThunk(String(id)));
   }, [dispatch, id]);
-  return (
-    <SingleMovie {...movie} error={movieError} isLoading={movieIsLoading} />
-  );
+
+  return <SingleMovie />;
 }
